4966d526b04f002aae545d7d54fd04da31296ca1
dhermanna <dhermanna@gmail.com>
2020-04-23 04:31:34 +0000

    Fin de l'ossature du site

---
+++ resources/views/Layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="{{asset('/bootstrap/js/jquery-3.5.0.min.js')}}"> </script>
+    <link rel="stylesheet" href="{{asset('/bootstrap/css/bootstrap.min.css')}}">
+    <script src="{{asset('/bootstrap/js/bootstrap.min.js')}}"> </script>
+    <title>Laracarte</title>
+</head>
+<body>
+    @include('layouts/partials/_nav')
+    @yield('contenu')
+    @include('layouts/partials/_footer')
+</body>
+</html>
---
+++ resources/views/Layouts/partials/_nav.blade.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-expand-md navbar-dark navbar-static-top bg-dark">
+  <a class="navbar-brand" href="#">Laracarte</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Artisan</a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Planète</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown01">
+          <a class="dropdown-item" href="#">Laravel.com</a>
+          <a class="dropdown-item" href="#">Laravel.io</a>
+          <a class="dropdown-item" href="#">Laracast</a>
+          <a class="dropdown-item" href="#">Laranews</a>
+          <a class="dropdown-item" href="#">Larachat</a>
+          <a class="dropdown-item" href="#">Larajob</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Register</a>
+        </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
